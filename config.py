edad25ad35370ebb47b2c9a9313f77c2c2c5f3cc
hre <hre@orgadata.ag>
2024-09-27 10:04:44 +0200

    initialise config

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+import json
+
+CONFIG_FILE = './config.json'
+
+def load_config():
+    with open(CONFIG_FILE, 'r') as f:
+        return json.load(f)
+    
+CONFIG = load_config()
---
+++ main.py
@@ -0,0 +1,7 @@
+import asyncio
+import socketio
+import os
+from config import CONFIG
+
+SECRET = CONFIG['SECRET']
+SERVER_URL = "https://games.uhno.de"
